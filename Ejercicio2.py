dd8cd3a191f427d2ca362b8b040ec681ffc8a42a
Mortir87 <aitor@blanco.casa>
2026-01-02 15:43:22 +0100

    Cambios enunciado ejercicio2 y gitignore

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,4 +1,6 @@
-# Escribir un programa que, dado un número entero, muestre su valor absoluto. Recuerda que, para los números positivos su valor absoluto es igual al número (el valor absoluto de 52 es 52), mientras que, para los negativos, su valor absoluto es el (el valor absoluto de -52 es 52). (1 punto) 
+# Escribir un programa que, dado un número entero, muestre su valor absoluto. Recuerda que, 
+# para los números positivos su valor absoluto es igual al número (el valor absoluto de 52 es 52), 
+# mientras que, para los negativos, su valor absoluto es el (el valor absoluto de -52 es 52). (1 punto) 
 #	***Ejemplo de ejecución:***
 #	Número: -12
 #   Valor absoluto: 12
